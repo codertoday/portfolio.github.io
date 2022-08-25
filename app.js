a85d53a9be1d0bde1789bcf4b39cd19de34623ff
codertoday <pranati.jayanty@vit.edu.in>
2022-08-25 17:32:15 +0530

    upload

---
+++ app.js
@@ -0,0 +1,79 @@
+const header=document.querySelector("header");
+const firstskill=document.querySelector(".skill:first-child");
+const skcounters=document.querySelectorAll(".counter span");
+const progressbars=document.querySelectorAll(".skills svg circle");
+const mlsection=document.querySelector(".milestones");
+const mlcounters=document.querySelectorAll(".number span");
+
+
+window.addEventListener("scroll", () => {
+    if(!skillsplayed) skillsCounter();
+    if(!mlplayed) mlCounters();
+})
+
+// Sticky Navbar
+function stickyNavbar(){
+    header.classList.toggle("scrolled", window.pageYOffset>0);
+  
+}
+
+stickyNavbar();
+
+window.addEventListener("scroll",stickyNavbar);
+
+let sr= ScrollReveal({
+    duration: 2500,
+    distance:"60px",
+});
+
+sr.reveal(".showcase-info",{delay:600});
+sr.reveal(".showcase-img",{origin:"top",delay:700});
+
+function updateCount(num, Maxnum){
+    let currentnum=+num.innerText;
+    if(currentnum<Maxnum){
+        num.innerText= currentnum+1;
+        setTimeout(() =>{
+            updateCount(num,Maxnum);
+        }, 12);
+    }
+}
+
+let skillsplayed=false;
+
+function hasReached(el){
+    let Tp=el.getBoundingClientRect().top;
+    if(window.innerHeight >= Tp + el.offsetHeight)
+        return true;
+   return false;
+}
+function skillsCounter(){
+    if(!hasReached(firstskill))  return;
+    skillsplayed=true;
+    skcounters.forEach((counter,i) => {
+        let target= +counter.dataset.target;
+        let strokevalue= 426 - 426 * (target/100);
+
+        progressbars[i].style.setProperty("--target", strokevalue);
+
+        setTimeout(() =>{
+            updateCount(counter,target);
+        }, 400);
+    });
+
+    progressbars.forEach(p => (p.style.animation = "progress 2s ease-in-out forwards"));
+}
+
+let mlplayed=false;
+
+function mlCounters(){
+    if(!hasReached(mlsection))  return;
+    mlplayed=true;
+    mlcounters.forEach(ctr => {
+        let target= +ctr.dataset.target;
+        
+    setTimeout(() =>{
+        updateCount(ctr, target);
+    },400);
+    });
+}
